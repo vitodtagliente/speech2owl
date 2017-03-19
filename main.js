6a1b412530c884c13d7dc1d49fe355ab1f622bff
vitodtagliente <12domenico>
2017-03-19 15:12:53 +0100

    ontology upload api

--- main.js
+++ main.js
@@ -3,11 +3,53 @@ var express = require('express');
 var app = express();
 var path = require("path");
 var colors = require('colors');
+var formidable = require('formidable');
+var fs = require('fs');
 
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.post('/upload', function (req, res) {
+    // create an incoming form object
+    var form = new formidable.IncomingForm();
+
+    // specify that we want to allow the user to upload multiple files in a single request
+    form.multiples = false;
+
+    // store all uploads in the /uploads directory
+    form.uploadDir = path.join(__dirname, '/public/assets');
+
+    // every time a file has been uploaded successfully,
+    // rename it to it's orignal name
+    form.on('file', function(field, file) {
+        console.log( "Upload Request: ".yellow + file.name );
+        var filename = path.join(form.uploadDir, file.name);
+
+        if( fs.existsSync( filename ) )
+            fs.unlinkSync( filename );
+
+        fs.rename(file.path, filename);
+    });
+
+    // log any errors that occur
+    form.on('error', function(err) {
+        console.log( "UPLOAD::ERROR".red );
+        console.log('');
+        res.end("UPLOAD::ERROR");
+    });
+
+    // once all the files have been uploaded, send a response to the client
+    form.on('end', function() {
+        console.log( "UPLOAD::SUCCESS".green );
+        console.log('');
+        res.end("UPLOAD::SUCCESS");
+    });
+
+    // parse the incoming request containing the form data
+    form.parse(req);
 });
 
 app.get('/nlp', function (req, res) {
